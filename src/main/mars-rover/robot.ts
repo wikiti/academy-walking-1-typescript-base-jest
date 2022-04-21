73eaecc1192d38c87796b9f8045aae5349772058
ProdanSergey <prodan.sergey.od@gmail.com>
2022-04-21 16:06:29 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/mars-rover/robot.ts
+++ src/main/mars-rover/robot.ts
@@ -1,37 +1,62 @@
-class Direction {
-  private constructor(direction: Direction) {}
 
-  static map: Record<string, string> = {
-    N: "W",
-    W: "S",
-    S: "E",
-    E: "N",
+type DirectionSide = "W" | "S" | "E" | "N";
+export class Direction {
+  private constructor(private direction: DirectionSide) {}
+
+  current() {
+    return this.direction;
+  }
+
+  static create(side: DirectionSide) {
+    return new Direction(side);
+  }
+
+  static left(direction: Direction) {
+    const side = Direction.map[direction.current()];
+
+    return new Direction(side.left as DirectionSide);
+  }
+
+  static map = {
+    N: {
+      left: "W"
+    },
+    W: {
+      left: "S"
+    },
+    S: {
+      left: "E"
+    },
+    E: {
+      left: "N"
+    },
   };
 }
 
 export class Robot {
   private direction: Direction;
 
-  constructor(direction: Direction) {
-    this.direction = direction;
+  constructor(directionSide: DirectionSide) {
+    this.direction = Direction.create(directionSide);
   }
 
   turnLeft() {
-    if (this.direction == "N") {
-      this.direction = "W";
-      return;
-    }
-    if (this.direction == "W") {
-      this.direction = "S";
-      return;
-    }
-    if (this.direction == "S") {
-      this.direction = "E";
-      return;
-    }
+    this.direction = Direction.left(this.direction);
+    // if (this.direction == "N") {
+    //   this.direction = "W";
+    //   return;
+    // }
+    // if (this.direction == "W") {
+    //   this.direction = "S";
+    //   return;
+    // }
+    // if (this.direction == "S") {
+    //   this.direction = "E";
+    //   return;
+    // }
   }
 
   turnRight() {
-    this.direction = "E";
+    this.direction = Direction.create('E');
   }
 }
--- src/test/mars-rover/robot.test.ts
+++ src/test/mars-rover/robot.test.ts
@@ -1,9 +1,9 @@
-import { Robot } from "../../main/mars-rover/robot";
+import { Direction, Robot } from "../../main/mars-rover/robot";
 
 describe("Mars rover", () => {
   it("should have a direction", () => {
     const rover = new Robot("N");
-    expect(rover["direction"]).toEqual("N");
+    expect(rover["direction"]).toEqual(Direction.create('N'));
   });
 
   describe("Turn left", () => {
@@ -11,29 +11,45 @@ describe("Mars rover", () => {
       const rover = new Robot("N");
       rover.turnLeft();
 
-      expect(rover["direction"]).toEqual("W");
+      expect(rover["direction"]).toEqual(Direction.create('W'));
     });
 
     it("should turn left from west to south", () => {
       const rover = new Robot("W");
       rover.turnLeft();
 
-      expect(rover["direction"]).toEqual("S");
+      expect(rover["direction"]).toEqual(Direction.create('S'));
     });
 
     it("should turn left from south to east", () => {
       const rover = new Robot("S");
       rover.turnLeft();
 
-      expect(rover["direction"]).toEqual("E");
+      expect(rover["direction"]).toEqual(Direction.create('E'));
+    });
+
+    it("should turn left from south to east", () => {
+      const rover = new Robot("E");
+      rover.turnLeft();
+
+      expect(rover["direction"]).toEqual(Direction.create('N'));
     });
   });
 
+  describe("Turn Right", () => {
+    it("should turn to the right", () => {
+      const rover = new Robot("N");
+      rover.turnRight();
+  
+      expect(rover["direction"]).toEqual(Direction.create('E'));
+    });
 
-  it("should turn to the right", () => {
-    const rover = new Robot("N");
-    rover.turnRight();
-
-    expect(rover["direction"]).toEqual("E");
-  });
+    it("should turn to the right", () => {
+      const rover = new Robot("E");
+      rover.turnRight();
+  
+      expect(rover["direction"]).toEqual(Direction.create('S'));
+    });
+  })
+  
 });
