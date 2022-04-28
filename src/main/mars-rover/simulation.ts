b0c6fe7ff37538cf623a20135778292f8868ac4b
Selcuk Baris <selcuk.baris@cazoo.co.uk>
2022-04-28 14:46:35 +0100

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/main/mars-rover/simulation.ts
@@ -0,0 +1,6 @@
+export class Simulation {
+  constructor(_input: string) {}
+  simulate() {
+    return "0:0:N";
+  }
+}
--- src/test/mars-rover/simulation.test.ts
+++ src/test/mars-rover/simulation.test.ts
@@ -1,7 +1,14 @@
+import { Simulation } from "../../main/mars-rover/simulation";
+
 describe("Simulation", () => {
   it("simulates an empty list of commands", () => {
     const simulation = new Simulation("");
 
     expect(simulation.simulate()).toEqual("0:0:N");
+  });
+  it("simulates rotating once to the left", () => {
+    const simulation = new Simulation("L");
+
+    expect(simulation.simulate()).toEqual("0:0:E");
   });
 });
